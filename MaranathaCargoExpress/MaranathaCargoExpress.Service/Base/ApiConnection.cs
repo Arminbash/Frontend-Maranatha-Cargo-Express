0dcdda5d19240b701c881f99981609235a78df9f
Johnny Arcia <cosmicvanbuuren@hotmail.com>
2022-01-07 16:43:51 -0600

    Api methods

---
+++ MaranathaCargoExpress/MaranathaCargoExpress.Service/Base/ApiConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaranathaCargoExpress.Service.Base
+{
+    public class ApiConnection
+    {
+        //Url de la API sin el Swageer
+        public static string ApiUrl = "http://localhost:5100/";
+
+        public static class EndPoints
+        {
+            public static string Cliente = "api/Cliente";
+            public static string Empelado = "api/Empelado";
+            public static string Rol = "api/Rol";
+            public static string Persona = "api/Persona";
+            public static string Traduccion = "api/Traduccion";
+            public static string Usuario = "api/Usuario";
+            public static string TipoCliente = "api/TipoCliente";
+            public static string TipoPersona = "api/TipoPersona";
+        }
+    }
+}
---
+++ MaranathaCargoExpress/MaranathaCargoExpress.Service/Base/GenericMethodsAPIClient.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaranathaCargoExpress.Service.Base
+{
+    public class GenericMethodsAPIClient<T> where T : class
+    {
+        public async Task<T> Get(string endPoint)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiConnection.ApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync(endPoint);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                return await Task.FromResult<T>(null);
+            }
+        }
+
+        public async Task<T> GetById(string endPoint, int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiConnection.ApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync(endPoint+ $"/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                return await Task.FromResult<T>(null);
+            }
+        }
+
+        public async Task<T> Post(string endPoint,object value)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiConnection.ApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.PostAsJsonAsync(endPoint,value);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                return await Task.FromResult<T>(null);
+            }
+        }
+
+        public async Task<T> Put(string endPoint,int id, object value)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiConnection.ApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.PutAsJsonAsync(endPoint + $"/{id}", value);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                return await Task.FromResult<T>(null);
+            }
+        }
+
+        public async Task<T> Delete(string endPoint, int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ApiConnection.ApiUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.DeleteAsync(endPoint + $"/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<T>();
+                }
+                return await Task.FromResult<T>(null);
+            }
+        }
+
+    }
+}
