9f7dabebef236ec284d9f0b27b411317566fca82
Johnny Arcia <cosmicvanbuuren@hotmail.com>
2022-01-06 19:56:42 -0600

    gitignore

--- Sistema-Envios---Maranatha-Cargo-Express-Frontend/app_frontend_maranatha_cargo_express/src/contexto/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import sesionUsaurioReducer from "./sesionUsuarioReducer"
-
-
-export const mainReducer = ( {sesionUsuario}, action) => {
-    return {
-        sesionUsuario : sesionUsaurioReducer(sesionUsuario,action)
-    }
-}
