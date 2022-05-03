84fa59fc69ae384030a26fbb92307328aef1a99b
luchohack15 <48897720+luchohack15@users.noreply.github.com>
2022-05-02 18:38:01 -0600

    Tipo Cliente
    Crud de tipo Cliente

---
+++ MaranathaCargoExpress/MaranathaCargoExpress.Service/Service/TypeClientService.cs
@@ -0,0 +1,44 @@
+﻿using MaranathaCargoExpress.Service.Base;
+using MaranathaCargoExpress.Service.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaranathaCargoExpress.Service.Service
+{
+    public class TypeClientService
+    {
+        public async Task<List<TipoClienteDto>> ListTypeClient(string token)
+        {
+            var resquest = await GenericMethodsAPIClient<List<TipoClienteDto>>.Get(ApiConnection.EndPoints.TipoCliente + "ObtenerTipoCliente", token);
+            return resquest;
+        }
+        public async Task<TipoClienteDto> AddTypeClientAsync(TipoClienteDto tipoClienteDto, string token)
+        {
+            tipoClienteDto.Estado = true;
+            var request = await GenericMethodsAPIClient<TipoClienteDto>.Post(ApiConnection.EndPoints.TipoCliente + "CreateTipoCliente", tipoClienteDto,token);
+            return request;
+        }
+        public async Task<TipoClienteDto> GetTipoCliente(int id, string token)
+        {
+            var request = await GenericMethodsAPIClient<TipoClienteDto>.Get(ApiConnection.EndPoints.TipoCliente + "ObtenerTipoCliente/" + id.ToString(), token);
+            return request;
+        }
+        public async Task<TipoClienteDto> UpdateTypeClient(TipoClienteDto tipoClienteDto, string token)
+        {
+            var request = await GenericMethodsAPIClient<TipoClienteDto>.Put(ApiConnection.EndPoints.TipoCliente + "UpdatetipoCliente",null,tipoClienteDto, token);
+            return request;
+        }
+
+        public async Task<TipoClienteDto> DeleteTypeClient(int id, string token)
+        {
+            var request = await GenericMethodsAPIClient<TipoClienteDto>.Get(ApiConnection.EndPoints.TipoCliente + "ObtenerTipoCliente/" + id.ToString(), token);
+            request.Estado = false;
+            return await UpdateTypeClient(request, token);
+
+        }
+       
+    }
+}
---
+++ MaranathaCargoExpress/MaranathaCargoExpress.Web/Controllers/TypeClientController.cs
@@ -0,0 +1,72 @@
+﻿using MaranathaCargoExpress.Service.Service;
+using MaranathaCargoExpress.Service.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaranathaCargoExpress.Web.Controllers
+{
+    [Authorize]
+    public class TypeClientController : Controller
+    {
+        TypeClientService _typeClientService = new TypeClientService();
+        // GET: TypeClient
+        public ActionResult Index()
+        {
+            ViewBag.EmployeeName = SessionHelper.GetName();
+            ViewBag.UserName = SessionHelper.GetUserName();
+            ViewBag.Email = SessionHelper.GetEmail();
+            return View();
+        }
+        public ActionResult TypeClient()
+        {
+            ViewBag.EmployeeName = SessionHelper.GetName();
+            ViewBag.UserName = SessionHelper.GetUserName();
+            ViewBag.Email = SessionHelper.GetEmail();
+            return View();
+
+        }
+        [HttpPost]
+        public JsonResult AddTypeClient (TipoClienteDto tipoClienteDto)
+        {
+            var request = _typeClientService.AddTypeClientAsync(tipoClienteDto, SessionHelper.GetToken());
+            if (request.Result == null)
+            {
+                return new BadRequest();
+            }
+            return Json(request.Result, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult ListTypeClient()
+        {
+            var request = _typeClientService.ListTypeClient(SessionHelper.GetToken());
+            if (request.Result == null)
+            {
+                return new BadRequest();
+            }
+            return Json(JsonSerializer.Serialize(request.Result), JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public  JsonResult DeleteTypeClient(int Id)
+        {
+            var request = _typeClientService.DeleteTypeClient(Id, SessionHelper.GetToken());
+            if (request.Result == null)
+            {
+                return new BadRequest();
+            }
+            return Json(request.Result, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult UpdateTypeClient(TipoClienteDto tipoClienteDto)
+        {
+            var request = _typeClientService.UpdateTypeClient(tipoClienteDto, SessionHelper.GetToken());
+            if (request.Result == null)
+            {
+                return new BadRequest();
+            }
+            return Json(request.Result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
