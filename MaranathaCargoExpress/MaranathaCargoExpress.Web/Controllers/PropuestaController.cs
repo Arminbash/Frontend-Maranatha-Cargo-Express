f383b64bdce2b9c0e14a631b3a336b6be578516d
asaelz <asael_zunigalml@yahoo.es>
2022-06-09 00:05:17 -0600

    Uploading Package View
    Uploading Package Deliver View.
    
    1 tab added, waiting for part 2 or pasting same structure

---
+++ MaranathaCargoExpress/MaranathaCargoExpress.Web/Controllers/PropuestaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaranathaCargoExpress.Web.Controllers
+{
+    public class PropuestaController : Controller
+    {
+        // GET: Propuesta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
